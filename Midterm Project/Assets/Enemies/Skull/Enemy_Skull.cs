8ff06f949de830d7e2539241c7b589e44f5a8a6d
corwschm <corwschm@iu.edu>
2020-10-27 17:32:59 -0400

    Update 10/27 5:31

---
+++ Midterm Project/Assets/Enemies/Skull/Enemy_Skull.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Skull : MonoBehaviour
+{
+    [SerializeField] public float _hp;
+	[SerializeField] private float _runSpeed = 10f;
+	[Range(0, .3f)] [SerializeField] private float _Smoothing = .05f;
+	[SerializeField] private LayerMask _PlatformMask;
+	[SerializeField] private Transform _PlatformCheckLeft;
+	[SerializeField] private Transform _PlatformCheckRight;
+	const float _PlatformCheckRadius = .05f;
+	private Rigidbody2D _RigidBody;
+	public Animator _animator;
+    private bool _active;
+	private Vector3 _CurrentSpeed = Vector3.zero;
+	private string _facing = "Right";
+
+    private void Awake()
+	{
+		_RigidBody = GetComponent<Rigidbody2D>();
+	}
+    
+    void Start()
+    {
+        _active = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        
+        // Check if there is platform to left
+		Collider2D[] leftColliders = Physics2D.OverlapCircleAll(_PlatformCheckLeft.position, _PlatformCheckRadius, _PlatformMask);
+		if (leftColliders.Length == 0) _facing = "Right";
+		Collider2D[] rightColliders = Physics2D.OverlapCircleAll(_PlatformCheckRight.position, _PlatformCheckRadius, _PlatformMask);
+		if (rightColliders.Length == 0) _facing = "Left";
+
+        if (_active)
+        {
+            _animator.SetBool("Active", true);
+            int direction = 0;
+            if (_facing == "Right") direction = 1;    
+            else direction = -1;  
+
+            // Call Move() and reset _jump
+            Move(direction * _runSpeed * Time.fixedDeltaTime);
+        }
+        else{
+            _animator.SetBool("Active", true);
+        }
+    }
+
+    public void Move(float move)
+	{
+		// Movement
+		Vector3 targetVelocity = new Vector2(move * 10f, _RigidBody.velocity.y);
+		_RigidBody.velocity = Vector3.SmoothDamp(_RigidBody.velocity, targetVelocity, ref _CurrentSpeed, _Smoothing);
+
+		// Facing
+		if (move > 0) 
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            _facing = "Right";
+        }
+		else if (move < 0) 
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            _facing = "Left";
+        }
+	}
+}
---
+++ Midterm Project/Assets/Player_Character/PlayerController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float _JumpSpeed = 900f;
+	[SerializeField] private float _runSpeed = 40f;
+	[Range(0, .3f)] [SerializeField] private float _Smoothing = .05f;
+	[SerializeField] private LayerMask _PlatformMask;
+	[SerializeField] private Transform _PlatformCheck;
+	private float _hDirection;
+    bool _jump = false;
+	const float _PlatformCheckRadius = .05f;
+	private bool _OnPlatform;
+	private Rigidbody2D _RigidBody;
+	private string _facing = "Right";
+	public Animator _animator;
+	private Vector3 _CurrentSpeed = Vector3.zero;
+
+	private void Awake()
+	{
+		_RigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update() 
+	{
+		// Get Inputs
+        _hDirection = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump")) _jump = true;
+		if (Input.GetKeyDown("x"))
+		{
+			if (_facing == "Right") 
+			{
+				gameObject.transform.Find("HitRight").GetComponent<GameObject>().SetActive(true);
+			}
+			else 
+			{
+				gameObject.transform.Find("HitLeft").GetComponent<GameObject>().SetActive(true);
+			}
+		}
+
+		// Change animation for running
+		_animator.SetFloat("Speed", Mathf.Abs(_hDirection));
+
+		// Check if falling to change animation
+		if (_RigidBody.velocity.y < 0) _animator.SetBool("Falling", true);
+		else _animator.SetBool("Falling", false);
+
+
+	}
+
+	private void FixedUpdate()
+	{
+		bool wasGrounded = _OnPlatform;
+		_OnPlatform = false;
+
+		// Check for platforms under Player
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(_PlatformCheck.position, _PlatformCheckRadius, _PlatformMask);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				_OnPlatform = true;
+				if (!wasGrounded)
+				{
+					// End Jump animation
+					_animator.SetBool("Jump", false);
+				}
+			}
+		}
+
+		// Call Move() and reset _jump
+        Move(_hDirection * _runSpeed * Time.fixedDeltaTime);
+        _jump = false;
+	}
+
+	public void Move(float move)
+	{
+		// Movement
+		Vector3 targetVelocity = new Vector2(move * 10f, _RigidBody.velocity.y);
+		_RigidBody.velocity = Vector3.SmoothDamp(_RigidBody.velocity, targetVelocity, ref _CurrentSpeed, _Smoothing);
+
+		// Facing
+		if (move > 0) 
+		{
+			GetComponent<SpriteRenderer>().flipX = false;
+			_facing = "Right";
+		}
+		else if (move < 0) 
+		{
+			GetComponent<SpriteRenderer>().flipX = true;
+			_facing = "Left";
+		}
+
+		// Jumping
+		if (_OnPlatform && _jump)
+		{
+			_OnPlatform = false;
+			_RigidBody.AddForce(new Vector2(0f, _JumpSpeed));
+			_animator.SetBool("Jump", true);
+		}
+	}
+}
