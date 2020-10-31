a8197a9a2d30d046cd8be5270586376f3dadd530
corwschm <corwschm@iu.edu>
2020-10-30 21:33:06 -0400

    Much Progress on Character Controller

--- midterm-project-Centricus/Assets/Player/PlayerController.cs
+++ midterm-project-Centricus/Assets/Player/PlayerController.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Inspector-Editable Values
     public Animator animator;
     public LayerMask platformMask;
     public LayerMask enemyMask;
-    public Vector3 velocity;
-    public bool grounded;
-    public bool wasGrounded;
-    public bool jumping;
-    public bool attacking;
-    public float hInput;
+    public float platformCheckDistance;
     public float hSpeed;
     public float maxHSpeed;
     public float jumpSpeed;
     public float gravity;
+
+    // Internal Values
     string facing = "Right";
+    Vector3 velocity;
+    bool grounded;
+    bool wasGrounded;
+    bool jumping;
+    bool attacking;
+    float hInput;
 
 
     // Start is called before the first frame update
@@ -35,7 +39,7 @@ public class PlayerController : MonoBehaviour
 
         // Getting inputs
         hInput = Input.GetAxis("Horizontal");
-        jumping = Input.GetButtonDown("Z");
+        jumping = Input.GetButtonDown("Jump");
 
 
         // End Jump
@@ -58,7 +62,7 @@ public class PlayerController : MonoBehaviour
     // Returns true if there is a platform immediately underneath the player
     bool CheckPlatformBelow()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.down, 0.1f, platformMask))
+        if (Physics2D.Raycast(transform.position, Vector2.down, platformCheckDistance, platformMask))
         {
             return true;
         }
@@ -71,7 +75,7 @@ public class PlayerController : MonoBehaviour
     // Returns true if there is a platform immediately above the player
     bool CheckPlatformAbove()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.up, 0.1f, platformMask))
+        if (Physics2D.Raycast(transform.position, Vector2.up, platformCheckDistance, platformMask))
         {
             return true;
         }
@@ -110,6 +114,9 @@ public class PlayerController : MonoBehaviour
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
 
+        // Horizontal movement
+        velocity.x = hInput * hSpeed;
+
         // Speed constraints
         if (velocity.x > maxHSpeed)
         {
@@ -120,15 +127,24 @@ public class PlayerController : MonoBehaviour
             velocity.x = -1 * maxHSpeed;
         }
 
+        // Gravity
         velocity.y -= gravity;
 
-        // Falling animation
+        // Stop on the ground
+        if (grounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+
+        // Animation
         if (velocity.y < 0)
         {
             animator.SetBool("Jumping", false);
             animator.SetBool("Falling", true);
         }
+        animator.SetFloat("hSpeed", Mathf.Abs(velocity.x));
         
+        velocity.z = 0;
         transform.position += velocity;
 
     }
